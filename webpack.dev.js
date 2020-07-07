9f87237d230cd63d7de9ca91ffde813c7e56f243
Alexander Rind <1217527+alex-rind@users.noreply.github.com>
2020-07-07 15:15:50 +0200

    allow hot reloading using docker volumes and webpack watchOPtions

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,6 +5,10 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
+    watchOptions: {
+      aggregateTimeout: 500, // delay before reloading
+      poll: 1000 // enable polling since fsevents are not supported in docker
+    },
     contentBase: './dist'
   },
 });
